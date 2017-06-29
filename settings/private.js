b4ebb35189a6169bd7bab8d39e9d393c6f70bd52
kawaiiDango <kawaiiDango@soKawaii.com>
2017-06-30 00:29:23 +0530

    template for api keys and stuff

---
+++ settings/private.js
@@ -0,0 +1,13 @@
+var private = {
+	botToken: "zz",
+	adminId: ["00000"],
+	SNKey: "zz",
+	analKey: "zz",
+    botName: "zz",
+
+	favouriteGroups: [
+    -100000000,
+    -100000000
+  ]
+}
+module.exports = private;
