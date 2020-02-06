aac09dce78c82c56194208231e86001b41a00004
kawaiiDango <1066519+kawaiiDango@users.noreply.github.com>
2020-02-06 17:39:03 +0530

    error messages were leaking the bot token

--- php/tokenHider.php
+++ php/tokenHider.php
@@ -1,7 +1,8 @@
 <?php
-	$token = "bot token";
-	$path = "/" . $_GET['l'];
-	$url = "https://api.telegram.org/file/bot" . $token . $path;
+    error_reporting(E_ERROR | E_PARSE);
+    $token = "bot token";
+    $path = "/" . $_GET['l'];
+    $url = "https://api.telegram.org/file/bot" . $token . $path;
     $extension = substr($path, strrpos($path,"."));
 
     header("Content-type: image/jpeg");
